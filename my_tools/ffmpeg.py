8f98116041af187a4250e8e1236f5e37e74443eb
serend1p1ty <zjli1997@163.com>
2021-08-09 14:55:22 +0800

    extend ffmpeg utils

--- my_tools/ffmpeg.py
+++ my_tools/ffmpeg.py
@@ -1,6 +1,8 @@
 """Library for image and video processing using ffmpeg."""
 
 import os
+import numpy as np
+import ffmpeg
 
 
 def save_to_movie(out_path, frame_path_format, fps=30, start_frame=0):
@@ -101,3 +103,63 @@ def stack_movies3x3(video_paths, out_path):
         str(out_path),
     ]
     os.system(" ".join(command))
+
+
+def ffmpeg_video_read(video_path, fps=None):
+    """Video reader based on FFMPEG.
+
+    This function supports setting fps for video reading.
+    The FPS can be set higher than the original FPS of the video.
+
+    Args:
+      video_path: A video file.
+      fps: Use specific fps for video reading. (optional)
+
+    Returns:
+      A `np.array` with the shape of [seq_len, height, width, 3]
+    """
+    assert os.path.exists(video_path), f"{video_path} does not exist!"
+    try:
+        probe = ffmpeg.probe(video_path)
+    except ffmpeg.Error as e:
+        print("stdout:", e.stdout.decode("utf8"))
+        print("stderr:", e.stderr.decode("utf8"))
+        raise e
+    video_info = next(stream for stream in probe["streams"] if stream["codec_type"] == "video")
+    width = int(video_info["width"])
+    height = int(video_info["height"])
+    stream = ffmpeg.input(video_path)
+    if fps:
+        stream = ffmpeg.filter(stream, "fps", fps=fps, round="up")
+    stream = ffmpeg.output(stream, "pipe:", format="rawvideo", pix_fmt="rgb24")
+    out, _ = ffmpeg.run(stream, capture_stdout=True)
+    out = np.frombuffer(out, np.uint8).reshape([-1, height, width, 3])
+    return out.copy()
+
+
+def ffmpeg_video_write(data, video_path, fps=25):
+    """Video writer based on FFMPEG.
+
+    Args:
+        data: A `np.array` with the shape of [seq_len, height, width, 3]
+        video_path: A video file.
+        fps: Use specific fps for video writing. (optional)
+    """
+    assert len(data.shape) == 4, f"input shape is not valid! Got {data.shape}!"
+    _, height, width, _ = data.shape
+    os.makedirs(os.path.dirname(video_path), exist_ok=True)
+    writer = (
+        ffmpeg.input(
+            "pipe:",
+            framerate=fps,
+            format="rawvideo",
+            pix_fmt="rgb24",
+            s="{}x{}".format(width, height),
+        )
+        .output(video_path, pix_fmt="yuv420p")
+        .overwrite_output()
+        .run_async(pipe_stdin=True)
+    )
+    for frame in data:
+        writer.stdin.write(frame.astype(np.uint8).tobytes())
+    writer.stdin.close()
